295d56df6fb664a6bc7fbfb7d7ac4ecada355dbd
bastijamal <personal78acc@gmail.com>
2024-04-02 09:29:26 +0400

    commit

---
+++ Darag/Program.cs
@@ -0,0 +1,74 @@
+﻿using System.Linq.Expressions;
+
+namespace Darag
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			Weapon weapon = new Weapon();
+
+			//weapon.Shoot(false);
+			weapon.ChangeFireMood(false);
+			weapon.GetRemainBulletCount(40, 10);
+			weapon.Reload(80, 20);
+
+
+			Console.WriteLine("---------------------------");
+
+
+			Console.WriteLine("0.Informasiya almaq ucun basin");
+			Console.WriteLine("1.Atesh achmaq");
+			Console.WriteLine("2.GetRemainBulletCount metodu");
+			Console.WriteLine("3.Reload Methodu");
+			Console.WriteLine("4.ChangeFiremood");
+			Console.WriteLine("5.Exit");
+
+
+			Console.WriteLine("Secim edin:");
+			string input = Console.ReadLine();
+			switch (input)
+			{
+				case "0":
+					Console.WriteLine($" capacity {weapon.CapacityBullet} current : {weapon.CurrentBullet} ");
+
+					break;
+
+				case "1":
+					bool isAutomatic = Convert.ToBoolean(Console.ReadLine());
+					weapon.Shoot(isAutomatic);
+					break;
+
+
+				case "2":
+					weapon.GetRemainBulletCount(30, 10);
+					
+					break;
+
+				case "3":
+					weapon.Reload(30, 10);
+
+					break;
+
+				case "4":
+					Console.WriteLine("Enter Mode to Change:");
+					bool IsChange = Convert.ToBoolean(Console.ReadLine());
+					weapon.ChangeFireMood(IsChange);
+
+					break;
+
+				case "5":
+
+					break;
+
+				default:
+					
+					break;
+			}
+
+
+
+		}
+	}
+}
---
+++ Darag/Weapon.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Darag
+{
+	internal class Weapon
+
+	{
+		public int CapacityBullet;
+		public int CurrentBullet;
+		bool Firemood; //true avtomatik,false tekli
+
+
+		public void Shoot(bool Firemood)
+		{
+			if (Firemood == true)
+			{
+				CurrentBullet-=1;
+				Console.WriteLine("1 gulle atildi");
+			}
+			else
+			{
+				CapacityBullet= 0;
+				Console.WriteLine("DARAG TEMMIZ BOSHADILDI");
+
+			}
+			
+		}
+
+		public void GetRemainBulletCount(int CapacityBullet, int CurrentBullet)
+		{
+			int a =  CapacityBullet-CurrentBullet;
+			Console.WriteLine($"Daragin tam dolmasi ucun lazim olan gulle sayi: {a}"); 
+
+		}
+
+
+		public void Reload(int CapacityBullet, int CurrentBullet)
+		{
+			//b tamamlamaq uchun lazim olan gulle sayidir (max tutum -mende olan)
+			int b = CapacityBullet - CurrentBullet;
+			CurrentBullet += b;
+			Console.WriteLine($"{b} dene Gulle Elave edilerek,Yeniden dolma bash verdi");
+
+		}
+
+		public void ChangeFireMood(bool Firemood)
+		{
+			if(Firemood== true) { 
+
+				Firemood = false;
+				Console.WriteLine("Atish modu ugurla deyishdirildi! Tekli oldu ");
+
+			}
+
+			else
+			{
+				Firemood= true;
+				Console.WriteLine("Atish modu ugurla deyishdirildi! Avtomatik oldu ");
+			}
+
+
+
+		}
+
+
+	}
+
+	
+
+	}
+
+
